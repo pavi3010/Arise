e93d0caf508810d7d068c6b938977e27a6a199ae
Mehanth <mehanth2310579@ssn.edu.in>
2025-09-19 04:47:50 +0530

    added speed insights for vercel

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useAuth } from '../contexts/AuthContext';
 import { logOut } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function Dashboard() {
   const { currentUser } = useAuth();
   const navigate = useNavigate();
@@ -39,6 +41,8 @@ function Dashboard() {
         </div>
       </header>
       
+      <SpeedInsights/>
+
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h2 className="text-xl font-semibold text-gray-800 mb-6">Your Learning Dashboard</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
